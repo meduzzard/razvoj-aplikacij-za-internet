0042d1e8132f91701fb9ce769182f5e8da48814d
meduzzard <meduzzard@gmail.com>
2024-05-11 19:41:30 +0200

    implemented MongoDB in app.js

--- backend/pametni-paketnik/app.js
+++ backend/pametni-paketnik/app.js
@@ -4,6 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// vključimo mongoose in ga povežemo z MongoDB
+var mongoose = require('mongoose');
+var mongoDB = "mongodb://127.0.0.1/pametni-paketnik";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
