9f1ca4af90e41723ec4f28dd8815e006e909e445
meduzzard <meduzzard@gmail.com>
2024-05-29 14:22:30 +0200

    simple test for backend

---
+++ backend/pametni-paketnik/tests/app.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app'); // Preverite, da je prava pot do vašega app.js
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('GET /', () => {
+  it('it should GET the home page', (done) => {
+    chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+  });
+});
